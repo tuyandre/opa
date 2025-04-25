a2378253994583fd1890e684f716b504cd35b41b
tuyandre <tuyandre2018@gmail.com>
2025-04-25 03:41:07 +0200

    statistics

---
+++ resources/views/frontend/components/performance.blade.php
@@ -0,0 +1,37 @@
+<!-- Fun Fatcs Start -->
+<section class="counterWrap">
+
+      <div class="container">
+        
+          <div class="row">
+              <div class="col-lg-3">
+                  <div class="counter">
+                      <h3>Happy Clients</h3>
+                      <h2 class="timer count-title count-number" data-to="50" data-speed="1500"></h2>
+                      <p class="count-text ">Clients</p>
+                  </div>
+              </div>
+              <div class="col-lg-3">
+                <div class="counter">
+                    <h3>People trained</h3>
+                    <h2 class="timer count-title count-number" data-to="150" data-speed="1500"></h2>
+                    <p class="count-text ">Students</p>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="counter">
+                    <h3>Services offered</h3>
+                    <h2 class="timer count-title count-number" data-to="10" data-speed="1500"></h2>
+                    <p class="count-text ">Services</p>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="counter">
+                    <h3>Issues solved</h3>
+                    <h2 class="timer count-title count-number" data-to="200" data-speed="1500"></h2>
+                    <p class="count-text ">Problems</p>
+                </div>
+            </div>
+          </div>
+      </div>
+  </section>
