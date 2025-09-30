3d13840e7a086eea5bacb68310250ef7c1ab2640
tuyandre <tuyandre2018@gmail.com>
2025-09-30 09:02:10 +0200

    counter

--- resources/views/frontend/layout/master.blade.php
+++ resources/views/frontend/layout/master.blade.php
@@ -214,6 +214,46 @@ $ROOT_FOLDER =$main_url.VariableConstants::ROOT_FOLDER;
     });
 </script>
 
+<script>
+    // Set the date we're counting down to (1 month from now)
+    const countDownDate = new Date("October 6, 2025 00:00:00").getTime();
+
+    // Update the countdown every 1 second
+    const x = setInterval(function() {
+        // Get today's date and time
+        const now = new Date().getTime();
+
+        // Find the distance between now and the countdown date
+        const distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result
+        document.getElementById("days").innerHTML = days.toString().padStart(2, '0') ;
+        document.getElementById("hours").innerHTML = hours.toString().padStart(2, '0');
+        document.getElementById("minutes").innerHTML = minutes.toString().padStart(2, '0');
+        document.getElementById("seconds").innerHTML = seconds.toString().padStart(2, '0');
+
+        // If the countdown is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("days").innerHTML = "00";
+            document.getElementById("hours").innerHTML = "00";
+            document.getElementById("minutes").innerHTML = "00";
+            document.getElementById("seconds").innerHTML = "00";
+            //remove apply now button and whole div
+            const ctaDiv = document.querySelector('.counter_div');
+            if (ctaDiv) {
+                //add hidden class
+                ctaDiv.style.display = 'none';
+            }
+        }
+    }, 1000);
+</script>
 </body>
 
 </html>
